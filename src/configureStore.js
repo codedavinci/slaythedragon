4c7dab38a12693beedb0a83c2439278f925c3daf
mac <codedavinci.now@gmail.com>
2017-10-18 21:00:58 -0700

    ConfigureStore with middleware

---
+++ src/configureStore.js
@@ -0,0 +1,14 @@
+import  { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import rootReducer from './reducers'
+
+export default (initialState) => {
+  const middleware = process.env.NODE_ENV !== 'production' ? [thunk, createLogger()] : [ thunk]
+
+  const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore)
+
+  const store = createStoreWithMiddleware(rootReducer, initialState)
+
+  return store
+}
