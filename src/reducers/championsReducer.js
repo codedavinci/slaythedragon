e604dc5b4734d004130f7196cbf78f53c8836135
mac <codedavinci.now@gmail.com>
2017-10-23 16:19:20 -0700

    Champions Reducers

---
+++ src/reducers/championsReducer.js
@@ -0,0 +1,31 @@
+const initialState = {
+  champions: [],
+  error: false,
+  isLoading: false
+}
+
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case "GET_CHAMPIONS_BEGIN":
+      return { 
+        ...state,
+        isLoading: true
+      }
+    case "GET_CHAMPIONS_SUCCESS":
+      return {
+        ...state,
+        champions: {...action.champs },
+        isLoading: false
+      }
+    case "GET_CHAMPIONS_ERROR":
+      return {
+        ...state,
+        error: action.err.message,
+        isLoading: false
+      }
+    default:
+      return state
+  }
+
+}
