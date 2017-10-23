4e6e7d6a0b515049ea026c9ad35f1b72ab50ef39
mac <codedavinci.now@gmail.com>
2017-10-23 16:18:52 -0700

    Main connected component

--- src/components/content/Main.js
+++ src/components/content/Main.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import './main.css'
+import { connect } from 'react-redux'
+import InfoList from './InfoList'
+import { Error, Loader } from '../common'
 
 
 
-export default  () => {
+const Main = ({ champs }) => {
+  const { isLoading, error, champions } = champs
+  
+  if(error) { return <Error err={error} />}
   return (
     <div className="main-container">
+      {  isLoading 
+        ? <Loader /> 
+        : <InfoList champs={champions} />
+      }
     </div>
   )
 }
 
+
+export default connect(state => ({ champs: {...state.champs}}))(Main)
