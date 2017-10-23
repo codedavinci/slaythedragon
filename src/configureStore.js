e255eecb2f536ca80cacdb3a4201b38b59cde872
mac <codedavinci.now@gmail.com>
2017-10-23 16:17:41 -0700

    Create mock

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,12 +1,14 @@
-import  { createStore, applyMiddleware } from 'redux'
+import  { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 import rootReducer from './reducers'
 
 export default (initialState) => {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
   const middleware = process.env.NODE_ENV !== 'production' ? [thunk, createLogger()] : [ thunk]
 
-  const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore)
+  const createStoreWithMiddleware = comcseEnhancers(applyMiddleware(...middleware))(createStore)
 
   const store = createStoreWithMiddleware(rootReducer, initialState)
 
